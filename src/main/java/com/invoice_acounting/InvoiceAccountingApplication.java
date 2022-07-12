e4a25a4415b1e9f11a33d1eed0bb6152909dbbdc
quickbookteam <newdevelop420@gmail.com>
2022-07-12 15:34:12 +0530

    third commit
    commit

--- src/main/java/com/invoice_acounting/InvoiceAccountingApplication.java
+++ src/main/java/com/invoice_acounting/InvoiceAccountingApplication.java
@@ -1,13 +1,36 @@
 package com.invoice_acounting;
 
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.intuit.ipp.services.DataService;
+import com.invoice_acounting.config.QuickBookIntegration;
 
 @SpringBootApplication
 public class InvoiceAccountingApplication {
+//	@Autowired
+//	QuickBookIntegration quickBookIntegration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceAccountingApplication.class, args);
-	}
+		BasicConfigurator.configure();
 
+	}
+//	@Bean
+//	public DataService getDataService()
+//	{
+//		try {
+//			return quickBookIntegration.demo();
+//		} catch (Exception e) {
+//			
+//			e.printStackTrace();
+//			return null;
+//		}
+//		 
+//	}
 }
+
+
